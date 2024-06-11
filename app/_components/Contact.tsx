cda53a72456fe3fbbef030989286ff1330055302
Anish Chaulagain <116931135+anishchaulagain@users.noreply.github.com>
2024-06-11 17:39:00 +0545

    Update Contact.tsx

--- app/_components/Contact.tsx
+++ app/_components/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
 
 
-        <section className="bg-gray-100 " >
+        <section className="bg-gray-100 " id="contact" >
             <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
                     <div className="lg:col-span-2 lg:py-12">
